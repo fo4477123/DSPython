d5377c42aa36a25f42e00d732b55055bce1e66f1
Raider <fo4477123@gmail.com>
2021-02-06 17:38:36 +0800

    HW8 Update

--- D08/D08.py
+++ D08/D08.py
@@ -10,5 +10,19 @@ rank_list = [8,1,5,4,7,6,2,3]
 
 myopia_list = [True,True,False,False,True,True,False,False]
 
-np.dtype({'names':('name','sex','weight','rank','myopia'),'format':(np.unicode,
-np.unicode,np.float,np.int,np.bool)})
+dt = np.dtype({'names':('Name', 'num1', 'num2', 'True'), 'formats':((np.str_, 5), np.int32, int, 'U3')})
+newArray = np.dtype({'names':('Name','sex','weight','rank','myopia'),'formats':((np.str_, 5),(np.str_, 5),'f','i','?')})
+a = np.zeros(8,dtype=newArray)
+
+for i in range(len(name_list)):
+    a[i]['Name'] = name_list[i]
+    a[i]['sex'] = sex_list[i]
+    a[i]['weight'] = weight_list[i]
+    a[i]['rank'] = rank_list[i]
+    a[i]['myopia'] = myopia_list[i]
+
+print(np.average(a['weight']))
+
+print('boy : 平均體重'+ str(np.average(a[a['sex']=='boy']['weight'])))
+
+print('girl : 平均體重'+ str(np.average(a[a['sex']=='girl']['weight'])))
