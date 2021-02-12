7658e6f457ab47792037883157277baa18898f1a
Raider <fo4477123@gmail.com>
2021-02-12 21:47:29 +0800

    HW12

---
+++ D12/D12.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+ts = pd.Series(np.random.randn(200),index=pd.date_range('1/1/2020',periods=200))
+
+ts=ts.cumsum()
+print(ts)
+ts.plot()
+plt.show()
+
+df = pd.read_csv('./D12/boston.csv')
+print(df)
+df.boxplot()
+
+plt.show()
+
+df.plot.scatter(x='NOX', y='DIS')
+
+plt.show()
+
