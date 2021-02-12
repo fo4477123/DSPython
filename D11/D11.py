972a03ae72b114ac1f96a2d7d05cc8dd061c72b7
Raider <fo4477123@gmail.com>
2021-02-12 21:25:40 +0800

    HW11

---
+++ D11/D11.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+_df = pd.DataFrame([['male', 'teacher'], ['male', 'engineer'], ['female', None], ['female', 'engineer']],columns=['Sex','Profession'])
+resultDF=_df.fillna('others')
+
+print(_df)
+print(resultDF)
