c9b1bee80a27f67aee0fc1f5f7edef8be67129cb
Raider <fo4477123@gmail.com>
2021-02-28 14:26:26 +0800

    fix error

--- D16/D16.py
+++ D16/D16.py
@@ -4,6 +4,6 @@ import numpy as np
 index = pd.date_range("1/1/2019", periods=20, freq='D')
 series = pd.Series(range(20), index=index)
 
-print(index.week)
+print(series.to_period('W'))
 
-print(np.mean(index.week))
+print(series.resample('W').mean())
