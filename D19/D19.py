e9a05b67bd90f6ca4395565fb02c5d24f3b4dd12
Raider <fo4477123@gmail.com>
2021-03-06 15:31:10 +0800

    update

--- D19/D19.py
+++ D19/D19.py
@@ -3,9 +3,9 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 
-x = np.arange(100)
+x = np.arange(20)
 plt.title = "Bar fig"
-plt.bar(x,np.random.rand(100))
+plt.bar(x,np.random.rand(20),facecolor='red', edgecolor='white')
 plt.xticks([]), plt.yticks([])
 plt.show()
 
